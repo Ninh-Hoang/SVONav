406f9ce53224bd8dbd7dddc9845a1bc385617a67
Hoang Gia Ninh <ninhhoangDev@gmail.com>
2021-04-08 14:30:29 +1200

    Create base class for SVONavVolume. Various SVO builds will be implement.

---
+++ Source/SVONav/Private/SVONavVolumeBase.cpp
@@ -0,0 +1,517 @@
+﻿#include "SVONavVolumeBase.h"
+#include "chrono"
+#include "DrawDebugHelpers.h"
+#include "SVONavUpdateOctreeTask.h"
+#include "Components/BrushComponent.h"
+#include "Async/ParallelFor.h"
+#include "Layers/LayersSubsystem.h"
+#include "Async/Async.h"
+#include "Builders/CubeBuilder.h"
+using namespace std::chrono;
+
+ASVONavVolumeBase::ASVONavVolumeBase(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer),
+	  VolumeOrigin(FVector::ZeroVector),
+	  VolumeExtent(FVector::ZeroVector)
+{
+	GetBrushComponent()->Mobility = EComponentMobility::Movable;
+	PrimaryActorTick.bCanEverTick = true;
+
+	RegisterAllActorTickFunctions(true, false);
+	CollisionQueryParams.bFindInitialOverlaps = true;
+	CollisionQueryParams.bTraceComplex = false;
+	CollisionQueryParams.TraceTag = "SVONavRasterize";
+}
+
+void ASVONavVolumeBase::UpdateOctree()
+{
+#if WITH_EDITOR
+	const auto UpdateStartTime = high_resolution_clock::now();
+#endif
+
+	Octree = CachedOctree;
+}
+
+bool ASVONavVolumeBase::BuildOctree()
+{
+	//init setup
+	Initialise();
+
+#if WITH_EDITOR
+	const auto StartTime = high_resolution_clock::now();
+#endif
+
+	InternalBuildOctree();
+	
+#if WITH_EDITOR
+	const float Duration = std::chrono::duration_cast<milliseconds>(high_resolution_clock::now() - StartTime).count() /
+        1000.0f;
+
+	// Octree info
+	int32 NumNodes = 0;
+	for (int32 I = 0; I < NumLayers; I++) NumNodes += Octree.Layers[I].Num();
+
+	UE_LOG(LogTemp, Display, TEXT("Generation Time : %f seconds"), Duration);
+	UE_LOG(LogTemp, Display, TEXT("Desired Volume Size : %fcm"), VolumeSize);
+	UE_LOG(LogTemp, Display, TEXT("Actual Volume Size : %fcm"), ActualVolumeSize);
+	UE_LOG(LogTemp, Display, TEXT("Voxel Size : %fcm"), VoxelSize);
+	UE_LOG(LogTemp, Display, TEXT("Voxel Exponent: %i"), VoxelExponent);
+	UE_LOG(LogTemp, Display, TEXT("Total Layers : %i"), NumLayers);
+	UE_LOG(LogTemp, Display, TEXT("Total Nodes : %i"), NumNodes);
+	UE_LOG(LogTemp, Display, TEXT("Total Leaves : %i"), Octree.Leaves.Num());
+	UE_LOG(LogTemp, Display, TEXT("Total Octree Bytes : %i"), NumBytes);
+
+	DebugDrawOctree();
+#endif
+	for (int32 i = 0; i < NumLayer; i ++)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Layer %i num: %i"), i, Octree.Layers[i].Num());
+	}
+	return true;
+}
+
+void ASVONavVolumeBase::InternalBuildOctree()
+{
+}
+
+void ASVONavVolumeBase::GetMortonVoxel(const FVector& Location, int32 LayerIndex, FIntVector& MortonLocation) const
+{
+	const FVector LocationLocal = Location - (VolumeOrigin - VolumeExtent);
+	const float Size = VoxelHalfSizes[LayerIndex] * 2;
+	MortonLocation.X = FMath::FloorToInt(LocationLocal.X / Size);
+	MortonLocation.Y = FMath::FloorToInt(LocationLocal.Y / Size);
+	MortonLocation.Z = FMath::FloorToInt(LocationLocal.Z / Size);
+}
+
+bool ASVONavVolumeBase::FindLink(layerindex_t LayerIndex, int32 NodeIndex, uint8 Direction, FSVONavLink& Link,
+                                 const FVector& NodeLocation)
+{
+	TArray<FSVONavNode>& LayerNodes = Octree.Layers[LayerIndex];
+	FSVONavNode& TargetNode = LayerNodes[NodeIndex];
+
+	uint_fast32_t X, Y, Z;
+	morton3D_64_decode(TargetNode.MortonCode, X, Y, Z);
+
+	FIntVector S(static_cast<int32>(X), static_cast<int32>(Y), static_cast<int32>(Z));
+	S += Directions[Direction];
+	//do check range here
+
+	X = S.X;
+	Y = S.Y;
+	Z = S.Z;
+
+	const uint_fast64_t AdjacentCode = morton3D_64_encode(X, Y, Z);
+
+	int32 NeighbourNodeIndex;
+	if (GetNodeIndex_Hie(LayerIndex, AdjacentCode, NeighbourNodeIndex))
+	{
+		Link.SetLayerIndex(LayerIndex);
+		Link.SetNodeIndex(NeighbourNodeIndex);
+
+#if WITH_EDITOR
+		if (LayerIndex != 0)
+		{
+			FVector NeighbourLocation;
+			GetNodeLocation_Hie(LayerIndex, LayerNodes[NeighbourNodeIndex].MortonCode, NeighbourLocation);
+			DebugLinks.Add(FSVONavDebugLink(NodeLocation, NeighbourLocation, LayerIndex));
+		}
+#endif
+
+		return true;
+	}
+	Link.Invalidate();
+	return false;
+}
+
+void ASVONavVolumeBase::UpdateTaskComplete()
+{
+	UnlockOctree();
+
+#if WITH_EDITOR
+	// Run the debug draw on the game thread
+	AsyncTask(ENamedThreads::GameThread, [=]()
+	{
+		DebugDrawOctree();
+	});
+#endif
+}
+
+void ASVONavVolumeBase::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+	FProperty* Property = PropertyChangedEvent.Property;
+	const FString PropertyName = Property != nullptr ? Property->GetFName().ToString() : "";
+
+	const TSet<FString> CriticalProperties = {
+		"VolumeSize",
+        "VoxelSize"
+    };
+	const TSet<FString> DebugProperties = {
+		"bDisplayVolumeBounds",
+        "VolumeBoundsColor",
+        "bDisplayLayers",
+        "bDisplayLeaves",
+        "bDisplayLeafOcclusion",
+        "bDisplayNeighbourLink",
+        "LineScale",
+        "LayerColours",
+        "LeafOcclusionColour",
+        "bDisplayMortonCodes",
+        "MortonCodeColour",
+        "MortonCodeScale"
+    };
+	if (CriticalProperties.Contains(PropertyName))
+	{
+		Initialise();
+		DebugDrawOctree();
+	}
+	else if (DebugProperties.Contains(PropertyName))
+	{
+		DebugDrawOctree();
+	}
+}
+
+void ASVONavVolumeBase::PostEditUndo()
+{
+	Super::PostEditUndo();
+	Initialise();
+}
+
+void ASVONavVolumeBase::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+}
+
+void ASVONavVolumeBase::BeginPlay()
+{
+	CachedOctree = Octree;
+	OnUpdateComplete.BindUObject(this, &ASVONavVolumeBase::UpdateTaskComplete);
+	SetActorTickInterval(TickInterval);
+}
+
+void ASVONavVolumeBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!bOctreeLocked)
+	{
+		if (bUpdateRequested)
+		{
+			// Prevent any further requests until update task is complete
+			LockOctree();
+
+			// Execute UpdateOctree as background task
+			(new FAutoDeleteAsyncTask<FSVONavUpdateOctreeTask>(this, OnUpdateComplete))->StartBackgroundTask();
+
+			// Update complete
+			bUpdateRequested = false;
+
+#if WITH_EDITOR
+		}
+		else if (bDebugDrawRequested)
+		{
+			DebugDrawOctree();
+			bDebugDrawRequested = false;
+#endif
+		}
+	}
+}
+
+void ASVONavVolumeBase::PostRegisterAllComponents()
+{
+	Super::PostRegisterAllComponents();
+}
+
+void ASVONavVolumeBase::PostUnregisterAllComponents()
+{
+	Super::PostUnregisterAllComponents();
+}
+
+void ASVONavVolumeBase::EditorApplyTranslation(const FVector& DeltaTranslation, bool bAltDown, bool bShiftDown,
+                                               bool bCtrlDown)
+{
+	Super::EditorApplyTranslation(DeltaTranslation, bAltDown, bShiftDown, bCtrlDown);
+	Initialise();
+}
+
+void ASVONavVolumeBase::EditorApplyRotation(const FRotator& DeltaRotation, bool bAltDown, bool bShiftDown,
+                                            bool bCtrlDown)
+{
+	Super::EditorApplyRotation(DeltaRotation, bAltDown, bShiftDown, bCtrlDown);
+	Initialise();
+}
+
+void ASVONavVolumeBase::EditorApplyScale(const FVector& DeltaScale, const FVector* PivotLocation, bool bAltDown,
+                                         bool bShiftDown, bool bCtrlDown)
+{
+	Super::EditorApplyScale(DeltaScale, PivotLocation, bAltDown, bShiftDown, bCtrlDown);
+	Initialise();
+}
+
+void ASVONavVolumeBase::UpdateVolume()
+{
+	// Calculate the nearest integer exponent to fit the voxel size perfectly within the volume extents
+	VoxelExponent = FMath::RoundToInt(FMath::Log2(VolumeSize / (VoxelSize)));
+
+	NumLayers = VoxelExponent + 1;
+
+	// Build a list of voxel half-scale sizes for each layer
+	VoxelHalfSizes.Reset();
+	VoxelHalfSizes.Reserve(NumLayers);
+	for (int32 I = 0; I < NumLayers; I++) VoxelHalfSizes.Add(GetVoxelScale(I) * 0.5f);
+
+	ActualVolumeSize = GetActualVolumeSize();
+	UCubeBuilder* CubeBuilder = Cast<UCubeBuilder>(GEditor->FindBrushBuilder(UCubeBuilder::StaticClass()));
+	CubeBuilder->X = ActualVolumeSize;
+	CubeBuilder->Y = ActualVolumeSize;
+	CubeBuilder->Z = ActualVolumeSize;
+	CubeBuilder->Build(GetWorld(), this);
+
+	const FBox Bounds = GetComponentsBoundingBox(true);
+	Bounds.GetCenterAndExtents(VolumeOrigin, VolumeExtent);
+}
+
+void ASVONavVolumeBase::Serialize(FArchive& Ar)
+{
+	Super::Serialize(Ar);
+	Ar << Octree;
+	Ar << VoxelHalfSizes;
+	Ar << VolumeExtent;
+	NumBytes = Octree.GetSize();
+}
+
+void ASVONavVolumeBase::Initialise()
+{
+	Octree.Reset();
+	BlockedIndices.Empty();
+	NumBytes = 0;
+
+#if WITH_EDITOR
+	FlushDebugDraw();
+#endif
+
+	UpdateVolume();
+}
+
+void ASVONavVolumeBase::GetVolumeExtents(const FVector& Location, int32 LayerIndex, FIntVector& Extents) const
+{
+	const FBox ComponentBox = GetComponentsBoundingBox(true);
+	FVector ComponentOrigin;
+	FVector ComponentExtent;
+	ComponentBox.GetCenterAndExtents(ComponentOrigin, ComponentExtent);
+	const FVector LocationLocal = Location - ComponentOrigin - ComponentExtent;
+	const float Scale = VoxelHalfSizes[LayerIndex];
+	Extents.X = FMath::FloorToInt(LocationLocal.X / Scale);
+	Extents.Y = FMath::FloorToInt(LocationLocal.Y / Scale);
+	Extents.Z = FMath::FloorToInt(LocationLocal.Z / Scale);
+}
+
+FBox ASVONavVolumeBase::GetBoundingBox() const
+{
+	const FBoxSphereBounds Bounds = GetBrushComponent()->CalcBounds(ActorToWorld());
+	return Bounds.GetBox();
+}
+
+bool ASVONavVolumeBase::GetLink(const FVector& Location, FSVONavLink& Link)
+{
+	if (!IsWithinBounds(Location)) return false;
+
+	int32 LayerIndex = Octree.Layers.Num() - 2;
+	while (LayerIndex >= 0)
+	{
+		const TArray<FSVONavNode>& Layer = GetLayer_Hie(LayerIndex);
+		FIntVector Voxel;
+		GetMortonVoxel(Location, LayerIndex, Voxel);
+		const uint_fast64_t MortonCode = morton3D_64_encode(Voxel.X, Voxel.Y, Voxel.Z);
+		int32 NodeIndex;
+		if (GetNodeIndex_Hie(LayerIndex, MortonCode, NodeIndex))
+		{
+			const FSVONavNode Node = Layer[NodeIndex];
+
+			if (!Node.HasChildren())
+			{
+				Link.SetLayerIndex(LayerIndex);
+				Link.SetNodeIndex(NodeIndex);
+				Link.SetSubNodeIndex(0);
+				return true;
+			}
+			LayerIndex = Node.FirstChild.LayerIndex;
+		}
+		else if (LayerIndex == 0)
+		{
+			break;
+		}
+	}
+	return false;
+}
+
+void ASVONavVolumeBase::FlushDebugDraw() const
+{
+	if (!GetWorld()) return;
+	FlushPersistentDebugLines(GetWorld());
+	FlushDebugStrings(GetWorld());
+}
+
+void ASVONavVolumeBase::AddDebugNavPath(const FSVONavDebugPath DebugPath)
+{
+}
+
+void ASVONavVolumeBase::RequestOctreeDebugDraw()
+{
+	bDebugDrawRequested = true;
+	DebugDrawOctree();
+	bDebugDrawRequested = false;
+}
+
+void ASVONavVolumeBase::DebugDrawOctree()
+{
+}
+
+int32 ASVONavVolumeBase::GetSegmentNodeCount_Hie(layerindex_t LayerIndex) const
+{
+	return FMath::Pow(2, VoxelExponent - LayerIndex);
+}
+
+bool ASVONavVolumeBase::GetNodeIndex_Hie(layerindex_t LayerIndex, uint_fast64_t NodeMortonCode, int32& NodeIndex) const
+{
+	const auto& OctreeLayer = Octree.Layers[LayerIndex];
+	int32 Start = 0;
+	int32 End = OctreeLayer.Num() - 1;
+	int32 Mean = (Start + End) * 0.5f;
+
+	// Binary search by Morton code
+	while (Start <= End)
+	{
+		if (OctreeLayer[Mean].MortonCode < NodeMortonCode) Start = Mean + 1;
+		else if (OctreeLayer[Mean].MortonCode == NodeMortonCode)
+		{
+			NodeIndex = Mean;
+			return true;
+		}
+		else End = Mean - 1;
+		Mean = (Start + End) * 0.5f;
+	}
+	return false;
+}
+
+float ASVONavVolumeBase::GetVoxelScale(uint8 LayerIndex) const
+{
+	return VolumeExtent.X / FMath::Pow(2.0f, VoxelExponent) * FMath::Pow(2.0f, LayerIndex + 1);
+}
+
+bool ASVONavVolumeBase::GetNodeLocation_Hie(uint8 LayerIndex, uint_fast64_t MortonCode, FVector& Location) const
+{
+	const float Scale = VoxelHalfSizes[LayerIndex] * 2;
+	uint_fast32_t X, Y, Z;
+	morton3D_64_decode(MortonCode, X, Y, Z);
+	Location = VolumeOrigin - VolumeExtent + Scale * FVector(X, Y, Z) + FVector(Scale * 0.5f);
+	return true;
+}
+
+bool ASVONavVolumeBase::LinkNodeIsValid(const FSVONavLink& Link) const
+{
+	if (static_cast<int32>(Link.LayerIndex) >= Octree.Layers.Num()) return false;
+	return Link.IsValid() && static_cast<int32>(Link.NodeIndex) < Octree.Layers[Link.LayerIndex].Num();
+}
+
+void ASVONavVolumeBase::GetNeighbourLinks(const FSVONavLink& Link, TArray<FSVONavLink>& NeighbourLinks) const
+{
+	if (!LinkNodeIsValid(Link)) return;
+	const FSVONavNode& Node = GetNode(Link);
+	for (int32 I = 0; I < 6; I++)
+	{
+		const FSVONavLink& AdjacentLink = Node.Neighbours[I];
+		if (!AdjacentLink.IsValid()) continue;
+		if (!LinkNodeIsValid(AdjacentLink)) continue;
+		const FSVONavNode& AdjacentNode = GetNode(AdjacentLink);
+		if (!AdjacentNode.HasChildren())
+		{
+			NeighbourLinks.Add(AdjacentLink);
+			continue;
+		}
+
+		TArray<FSVONavLink> Links;
+		Links.Push(AdjacentLink);
+
+		while (Links.Num() > 0)
+		{
+			FSVONavLink CurrentLink = Links.Pop();
+			const FSVONavNode& CurrentNode = GetNode(CurrentLink);
+			if (!CurrentNode.HasChildren())
+			{
+				NeighbourLinks.Add(CurrentLink);
+				continue;
+			}
+			if (CurrentLink.GetLayerIndex() > 0)
+			{
+				for (const int32& ChildIndex : NodeOffsets[I])
+				{
+					FSVONavLink LinkChild = CurrentNode.FirstChild;
+					LinkChild.NodeIndex += ChildIndex;
+					const FSVONavNode& NodeChild = GetNode(LinkChild);
+					if (NodeChild.HasChildren()) Links.Emplace(LinkChild);
+					else NeighbourLinks.Emplace(LinkChild);
+				}
+			}
+			else
+			{
+				for (const int32& LeafIndex : LeafOffsets[I])
+				{
+					FSVONavLink LinkChild = AdjacentNode.FirstChild;
+					const FSVONavLeafNode& Leaf = Octree.Leaves[LinkChild.NodeIndex];
+					LinkChild.SubNodeIndex = LeafIndex;
+					if (!Leaf.GetSubNode(LeafIndex))
+					{
+						NeighbourLinks.Emplace(LinkChild);
+					}
+				}
+			}
+		}
+	}
+}
+
+int32 ASVONavVolumeBase::GetLayerNodeCount_Hie(layerindex_t LayerIndex) const
+{
+	return FMath::Pow(8, VoxelExponent - LayerIndex);
+}
+
+const FSVONavNode& ASVONavVolumeBase::GetNode(const FSVONavLink& Link) const
+{
+	return Octree.Layers[Link.LayerIndex][Link.NodeIndex];
+}
+
+FColor ASVONavVolumeBase::GetLayerColour(const int32 LayerIndex) const
+{
+	const float Ratio = LayerColours.Num() / static_cast<float>(NumLayer) * LayerIndex;
+	const int32 FirstIndex = FMath::FloorToInt(Ratio);
+	const int32 LastIndex = FMath::Min(FMath::CeilToInt(Ratio), LayerColours.Num() - 1);
+	const float Lerp = FMath::Fmod(Ratio, 1);
+	return FColor(
+		FMath::Lerp(LayerColours[FirstIndex].R, LayerColours[LastIndex].R, Lerp),
+		FMath::Lerp(LayerColours[FirstIndex].G, LayerColours[LastIndex].G, Lerp),
+		FMath::Lerp(LayerColours[FirstIndex].B, LayerColours[LastIndex].B, Lerp)
+	);
+}
+
+bool ASVONavVolumeBase::IsBlocked(const FVector& Location, float Size) const
+{
+	return GetWorld()->OverlapBlockingTestByChannel(
+		Location,
+		FQuat::Identity,
+		CollisionChannel,
+		FCollisionShape::MakeBox(FVector(Size + Clearance)),
+		CollisionQueryParams
+	);
+}
+
+bool ASVONavVolumeBase::IsBlocked(const FVector& Location, float Size, TArray<FOverlapResult>& OverlapResults) const
+{
+	return GetWorld()->OverlapMultiByChannel(
+		OverlapResults,
+		Location,
+		FQuat::Identity,
+		CollisionChannel,
+		FCollisionShape::MakeBox(FVector(Size + Clearance)),
+		CollisionQueryParams
+	);
+}
---
+++ Source/SVONav/Public/SVONavVolumeBase.h
@@ -0,0 +1,249 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "SVONavType.h"
+#include "GameFramework/Volume.h"
+#include "SVONavVolume.generated.h"
+
+DECLARE_DELEGATE(FSVONavUpdateOctreeDelegate);
+
+/**
+* Volume contains the octree and methods required for  navigation
+*/
+
+UCLASS(Blueprintable, meta=(DisplayName = "SVO Hierarchical Navigation Volume"))
+class SVONAV_API ASVONavVolumeBase : public AVolume
+{
+	GENERATED_BODY()
+
+public:
+	ASVONavVolumeBase(const FObjectInitializer& ObjectInitializer);
+
+	// The size of the volume. This will be approximated in order to support the requested voxel size
+	UPROPERTY(EditAnywhere, meta=(ClampMin = "0.000001"), DisplayName = "Desired Volume Size",
+		Category = "SVONav|Volume")
+	float VolumeSize = 200.0f;
+
+	// The minimum size of a leaf voxel in the X, Y and Z dimensions.
+	UPROPERTY(EditAnywhere, meta=(ClampMin = "0.000001"), DisplayName = "Minimum Voxel Size",
+		Category = "SVONav|Volume")
+	float VoxelSize = 200.0f;
+
+	// Which collision channel to use for object tracing during octree generation
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Volume")
+	TEnumAsByte<ECollisionChannel> CollisionChannel;
+
+	// The minimum distance away from any object traces to apply during octree generation
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Volume")
+	float Clearance = 0.f;
+
+	// How often to tick this actor to perform dynamic updates
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Volume")
+	float TickInterval = 0.2f;
+
+	// Draw distance for debug lines
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging")
+	float DebugDistance = 20000.f;
+
+	// Show the entire volume bounds
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging")
+	bool bDisplayVolumeBounds = false;
+
+	// The colour for the volume bounds debug draw
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging")
+	FColor VolumeBoundsColour = FColor(0, 128, 0, 64);
+
+	// Show the octree voxel node bounds
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging|Voxels")
+	bool bDisplayLayers = false;
+
+	// Show the octree layer bounds
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging|Voxels")
+	bool bDisplayLeaves = false;
+
+	// Show the occluded octree voxel sub-leaf bounds
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging|Voxels")
+	bool bDisplayLeafOcclusion = false;
+
+	// Show adjacency link between each node
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging|Voxels")
+	bool bDisplayNeighbourLink = false;
+
+	// The scaling factor for debug line drawing. Set to zero for fastest performance
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging|Voxels")
+	float LineScale = 0.0f;
+
+	// The scaling factor for debug line drawing. Set to zero for fastest performance
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging|Voxel")
+	TArray<FSVONavDebugVoxel> DebugVoxelList;
+
+	// The colours for debug drawing each layer. Colours added will be spread across a gradient
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging|Voxels|Colours")
+	TArray<FColor> LayerColours = {FColor::Magenta, FColor::Blue, FColor::Cyan, FColor::Green};
+
+	// The colour for leaf occlusion debug drawing
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging|Voxels|Colours")
+	FColor LeafOcclusionColour = FColor::Yellow;
+
+	// Show the morton codes within each voxel
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging|Morton Codes")
+	bool bDisplayMortonCodes = false;
+
+	// The colour for morton code debug drawing
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging|Morton Codes")
+	FColor MortonCodeColour = FColor::Magenta;
+
+	// The scaling factor for morton code debug drawing
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SVONav|Debugging|Morton Codes")
+	float MortonCodeScale = 1.0f;
+
+	// The number of voxel subdivisions calculated to meet the desired voxel size. Read-only
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SVONav|Info")
+	int32 VoxelExponent = 6;
+
+	// The actual volume size calculated to meet the desired volume size and voxel size. Read-only
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SVONav|Info")
+	float ActualVolumeSize = 200.0f;
+
+	// The number of layers created by the octree generation. Read-only
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SVONav|Info")
+	uint8 NumLayers = 0;
+
+	// The number of bytes used to store the generated octree. Read-only
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SVONav|Info")
+	int32 NumBytes = 0;
+
+	UFUNCTION()
+    void UpdateTaskComplete();
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+	virtual void PostEditUndo() override;
+#endif
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void PostRegisterAllComponents() override;
+	virtual void PostUnregisterAllComponents() override;
+	virtual void EditorApplyTranslation(const FVector& DeltaTranslation, bool bAltDown, bool bShiftDown,
+                                        bool bCtrlDown) override;
+
+	virtual void
+    EditorApplyRotation(const FRotator& DeltaRotation, bool bAltDown, bool bShiftDown, bool bCtrlDown) override;
+	virtual void EditorApplyScale(const FVector& DeltaScale, const FVector* PivotLocation, bool bAltDown,
+                                  bool bShiftDown, bool bCtrlDown) override;
+	virtual void Serialize(FArchive& Ar) override;
+
+	virtual void Initialise();
+	virtual bool BuildOctree();
+	virtual void UpdateOctree();
+
+	//update octree
+	void LockOctree() { bOctreeLocked = true; }
+	void UnlockOctree() { bOctreeLocked = false; }
+
+	//getter setter checker
+	bool OctreeValid() const { return NumLayers > 0; }
+	void GetVolumeExtents(const FVector& Location, int32 LayerIndex, FIntVector& Extents) const;
+	void GetMortonVoxel(const FVector& Location, int32 LayerIndex, FIntVector& MortonLocation) const;
+	FBox GetBoundingBox() const;
+	bool GetLink(const FVector& Location, FSVONavLink& Link);
+	float GetVoxelScale(uint8 LayerIndex) const;
+	const TArray<FSVONavNode>& GetLayer_Hie(uint8 LayerIndex) const { return Octree.Layers[LayerIndex]; };
+	const FSVONavNode& GetNode(const FSVONavLink& Link) const;
+	bool LinkNodeIsValid(const FSVONavLink& Link) const;
+	bool GetNodeLocation_Hie(uint8 LayerIndex, uint_fast64_t MortonCode, FVector& Location) const;
+	void GetNeighbourLinks(const FSVONavLink& Link, TArray<FSVONavLink>& NeighbourLinks) const;
+	int32 GetLayerCount_Hie() const {return Octree.Layers.Num();}
+	bool IsWithinBounds(const FVector Location) const { return GetBoundingBox().IsInside(Location); }
+
+	//debug draw
+	void FlushDebugDraw() const;
+	void AddDebugNavPath(const FSVONavDebugPath DebugPath);
+	void RequestOctreeDebugDraw();
+	virtual void DebugDrawOctree();
+	FColor GetLayerColour(const int32 LayerIndex) const;
+
+protected:
+	FSVONavOctree Octree;
+	FSVONavOctree CachedOctree;
+	TArray<float> VoxelHalfSizes;
+	TArray<TSet<uint_fast64_t>> BlockedIndices;
+	int32 NumLayer;
+
+	FVector VolumeOrigin;
+	FVector VolumeExtent;
+
+	FCollisionQueryParams CollisionQueryParams;
+
+	FSVONavUpdateOctreeDelegate OnUpdateComplete;
+	bool bOctreeLocked = false;
+	bool bUpdateRequested;
+	
+#if WITH_EDITOR
+	bool bDebugDrawRequested;
+	TArray<FSVONavDebugLink> DebugLinks;
+	TArray<FSVONavDebugPath> DebugPaths;
+	TArray<FSVONavDebugLocation> DebugLocations;
+#endif
+	
+	const FIntVector Directions[6] = {
+		FIntVector(1, 0, 0),
+        FIntVector(-1, 0, 0),
+        FIntVector(0, 1, 0),
+        FIntVector(0, -1, 0),
+        FIntVector(0, 0, 1),
+        FIntVector(0, 0, -1)
+    };
+
+	const FIntVector FirstChildOffsetDir[7] = {
+		FIntVector(1, 0, 0),
+        FIntVector(1, 1, 0),
+        FIntVector(0, 1, 0),
+        FIntVector(0, 1, 1),
+        FIntVector(1, 1, 1),
+        FIntVector(1, 0, 1),
+        FIntVector(0, 0, 1)
+    };
+
+	const int32 NodeOffsets[6][4] = {
+		{0, 4, 2, 6},
+		{1, 3, 5, 7},
+		{0, 1, 4, 5},
+		{2, 3, 6, 7},
+		{0, 1, 2, 3},
+		{4, 5, 6, 7}
+	};
+	
+	const int32 LeafOffsets[6][16] = {
+		{0, 2, 16, 18, 4, 6, 20, 22, 32, 34, 48, 50, 36, 38, 52, 54},
+		{9, 11, 25, 27, 13, 15, 29, 31, 41, 43, 57, 59, 45, 47, 61, 63},
+		{0, 1, 8, 9, 4, 5, 12, 13, 32, 33, 40, 41, 36, 37, 44, 45},
+		{18, 19, 26, 27, 22, 23, 30, 31, 50, 51, 58, 59, 54, 55, 62, 63},
+		{0, 1, 8, 9, 2, 3, 10, 11, 16, 17, 24, 25, 18, 19, 26, 27},
+		{36, 37, 44, 45, 38, 39, 46, 47, 52, 53, 60, 61, 54, 55, 62, 63}
+	};
+
+	//octree generate
+	virtual void UpdateVolume();
+	virtual void InternalBuildOctree();
+	virtual bool FindLink(layerindex_t LayerIndex, int32 NodeIndex, uint8 Direction, FSVONavLink& Link, const FVector& NodeLocation);
+	virtual bool GetNodeIndex_Hie(layerindex_t LayerIndex, uint_fast64_t NodeMortonCode, int32& NodeIndex) const;
+
+	bool IsBlocked(const FVector& Location, float Size) const;
+	bool IsBlocked(const FVector& Location, float Size, TArray<FOverlapResult>& OverlapResults) const;
+	
+	int32 GetLayerNodeCount_Hie(layerindex_t LayerIndex) const;
+	int32 GetSegmentNodeCount_Hie(layerindex_t LayerIndex) const;
+	virtual float GetActualVolumeSize() const { return FMath::Pow(2, VoxelExponent) * (VoxelSize); }
+
+	//debug draw
+	void DebugDrawVolume() const;
+	void DebugDrawVoxel(FVector Location, FVector Extent, FColor Colour) const;
+	void DebugDrawSphere(const FVector Location, const float Radius, const FColor Colour) const;
+	void DebugDrawMortonCode(FVector Location, FString String, FColor Colour) const;
+	void DebugDrawLeafOcclusion();
+	void DebugDrawNeighbourLink() const;
+	void DebugDrawBoundsMesh(FBox Box, FColor Colour) const;
+};
